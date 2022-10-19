525addfaf6d3cb8e02445267957a8ed695c3699b
rayssarayene <108935212+rayssarayene@users.noreply.github.com>
2022-10-19 12:42:05 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -43,3 +43,32 @@ function total(){
     }
     document.write("resultado: " + r);
 }
+
+function media(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let n4 = document.getElementById("n4").value;
+
+    let r = (Number(n1)+Number (n2)+Number (n3)+Number (n4))/4;
+    document.getElementById("resul").innerHTML = "Média:" + r;
+}
+function soma(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let n4 = document.getElementById("n4").value;
+
+    let r = (Number(n1)+Number (n2)+Number (n3)+Number (n4))/4;
+    document.getElementById("resul").innerHTML = "Média:" + r;
+}
+function subtrair(){
+    let n1 = document.getElementById("n1").value;
+    let n2 = document.getElementById("n2").value;
+    let n3 = document.getElementById("n3").value;
+    let n4 = document.getElementById("n4").value;
+
+    let r = (Number(n1)+Number (n2)+Number (n3)+Number (n4))/4;
+    document.getElementById("resul").innerHTML = "Média:" + r;
+}
+
