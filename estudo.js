adbba16d171f9982e8e568cad37d5a56ec7d4f01
rayssarayene <108935212+rayssarayene@users.noreply.github.com>
2022-10-19 12:45:48 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -60,7 +60,7 @@ function soma(){
     let n4 = document.getElementById("n4").value;
 
     let r = (Number(n1)+Number (n2)+Number (n3)+Number (n4))/4;
-    document.getElementById("resul").innerHTML = "Média:" + r;
+    document.getElementById("resul").innerHTML = "soma:" + r;
 }
 function subtrair(){
     let n1 = document.getElementById("n1").value;
@@ -68,7 +68,7 @@ function subtrair(){
     let n3 = document.getElementById("n3").value;
     let n4 = document.getElementById("n4").value;
 
-    let r = (Number(n1)+Number (n2)+Number (n3)+Number (n4))/4;
-    document.getElementById("resul").innerHTML = "Média:" + r;
+    let r = (Number(n1)-Number (n2)-Number (n3)-Number (n4))/4;
+    document.getElementById("resul").innerHTML = "subtrair:" + r;
 }
 
