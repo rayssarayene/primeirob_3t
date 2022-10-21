4826c57c5a75c47f624fae8071d5dc26b90356d4
rayssarayene <108935212+rayssarayene@users.noreply.github.com>
2022-10-21 09:08:24 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -31,14 +31,39 @@ function multiplica(){
      document.write("<br>");
 }
 
+
+
 function total(){
     let v = document.getElementById("valor").value;
     let j = document.getElementById("juros").value;
     let t = document.getElementById("meses").value;
+    
+    if(!Number(v)){
+     alert("o valor deve ser númerico.");
+     document.getElementById("valor").value = "";
+     document.getElementById("valor").focus();
+        return
+    }
+
+    if(!Number(j)){
+        alert("o juros deve ser númerico.");
+        document.getElementById("juros").value = "";
+        document.getElementById("juros").focus();
+           return
+       }
+
+       if(!Number(t)){
+        alert("o valor dos meses deve ser númerico.");
+        document.getElementById("meses").value = "";
+        document.getElementById("meses").focus();
+           return
+       }
+
+
     let r = 0;
     for(let i=1; i <= t;i++){
          r = v * (1+(j/100));
-         document.write("Mês " + " valor: " + r + "<br>");
+         document.write("Mês " + " valor: " + moeda(r) + "<br>");
          v = r;
     }
     document.write("resultado: " + r);
@@ -71,4 +96,5 @@ function subtrair(){
     let r = (Number(n1)-Number (n2)-Number (n3)-Number (n4))/4;
     document.getElementById("resul").innerHTML = "subtrair:" + r;
 }
+
 
